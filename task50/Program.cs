5fa9c2b02ccad006882e7ce5cefd1960be770e71
Юленька <chervyak_9@mail.ru>
2022-08-26 00:22:44 +0300

    Задача 50: показать элемент на заданной позиции

--- task50/Program.cs
+++ task50/Program.cs
@@ -9,17 +9,17 @@
 int[,] CreateMatrixRnd(int min, int max)
 {
     Random rnd = new Random();
-    int row = rnd.Next(1,10);                //пусть и размеры матрицы заранее не известны, так интереснее
-    int col = rnd.Next(2,10);                // двумерный, значит минимум 2 строки
-    int[,] matrix = new int[row,col];
-    
+    int row = rnd.Next(2, 10);                // пусть и размеры матрицы заранее не известны, так интереснее
+    int col = rnd.Next(1, 10);                // + двумерный, значит минимум 2 строки
+    int[,] matrix = new int[row, col];
+
     for (int i = 0; i < matrix.GetLength(0); i++)   //row
     {
         for (int j = 0; j < matrix.GetLength(1); j++)   //col
         {
-            matrix[i,j] = rnd.Next(min,max+1);
+            matrix[i, j] = rnd.Next(min, max + 1);
         }
-        
+
     }
     return matrix;
 }
@@ -28,24 +28,24 @@ void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        
-    Console.Write("[");
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        Console.Write($"{matrix[i,j],4}");
-        if (j != matrix.GetLength(1)-1) Console.Write(",");
-    }
-    Console.Write("]");
-    Console.WriteLine();
+
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4}");
+            if (j != matrix.GetLength(1) - 1) Console.Write(",");
+        }
+        Console.Write("]");
+        Console.WriteLine();
     }
 }
 
 //метод, который выводит значение элемента массива
-string ValueMatrixElement (int[,] matrix, int row, int col)
+string ValueMatrixElement(int[,] matrix, int row, int col)
 {
-    if (row>matrix.GetLength(0) || col>matrix.GetLength(1) || row<1 || col<1)  
-    return $"[{row},{col}] -> такого элемента в массиве нет";
-    else return $"Значение элемента массива [{row},{col}] -> {matrix[row-1,col-1]}";   // -1, для того чтобы пользователь не догадался, что нумерация элементов с 0
+    if (row > matrix.GetLength(0) || col > matrix.GetLength(1) || row < 1 || col < 1)
+        return $"[{row},{col}] -> такого элемента в массиве нет";
+    else return $"Значение элемента массива [{row},{col}] -> {matrix[row - 1, col - 1]}";   // -1, для того чтобы пользователь не догадался, что нумерация элементов с 0
 }
 
 //код
@@ -55,7 +55,7 @@ Console.Write("Введите номер стобца: ");
 int cl = Convert.ToInt32(Console.ReadLine());
 
 // не проверяем ввел ли пользователь корректное значение, т. к. при использовании метода получим нормальный ответ, что такого элемента нет в массиве
-int[,] mat = CreateMatrixRnd(0,10);
+int[,] mat = CreateMatrixRnd(0, 10);
 PrintMatrix(mat);
 Console.WriteLine();
 
