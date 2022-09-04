d0b3d7749e0be08fc75e55ec491819bd867bd384
Юленька <chervyak_9@mail.ru>
2022-09-05 00:08:04 +0300

    задача 64: ряд натуральных чисел от N до 1

---
+++ task64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+//метод с рекурсией
+void NatureNum(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num}");
+    if (num != 1) Console.Write(",");
+    NatureNum(num - 1);
+}
+
+//код
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1) Console.WriteLine("Введено некорректное значение");
+else
+{
+    Console.Write($"N = {number} -> ");
+    NatureNum(number);
+}
